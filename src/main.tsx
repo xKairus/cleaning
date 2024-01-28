577df92e3a3c25643b976ad1448117deae9c35c7
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-01-28 17:27:19 +0500

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+
+import "./styles/variables.css";
+import "./styles/normalize.css";
+import "./styles/fonts.css";
+import "./styles/fonts.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
