2710c3450b0afaf2d7c2f01ad54361ecb23db51c
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-01-28 19:24:07 +0500

    feat: add basic styles to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import logo from "./../../assets/Logo.svg";
+import callIcon from "./../../assets/call_icon.svg";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <img src={logo} alt="logo" />
-      <nav>
-        <ul>
-          <li>Home</li>
+      <nav className={styles.nav}>
+        <ul className={styles.nav__list}>
+          <li className={styles.active}>Home</li>
           <li>About</li>
           <li>Services</li>
           <li>Blog</li>
@@ -14,10 +16,12 @@ export default function Header() {
           <li>Contact</li>
         </ul>
       </nav>
-      <div>
-        <img src="" alt="call us" />
-        <span>Call us now</span>
-        <span>+1(243)421 4121</span>
+      <div className={styles.call}>
+        <img src={callIcon} alt="call us" />
+        <div className={styles.call__info}>
+          <span>Call us now</span>
+          <span>+1(243)421 4121</span>
+        </div>
       </div>
     </header>
   );
