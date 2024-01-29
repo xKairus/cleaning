86b0805dfab59abe2830fadd9c3bd092c7d0c123
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-01-29 14:23:59 +0500

    feat: add about component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Welcome from "./components/Welcome/Welcome";
 import Header from "./components/Header/Header";
+import About from "./components/About/About";
 
 function App() {
   return (
     <>
       <Header />
       <Welcome />
+      <About />
     </>
   );
 }
---
+++ src/components/About/About.tsx
@@ -0,0 +1,22 @@
+import styles from "./About.module.css";
+
+export default function About() {
+  return (
+    <section className={styles.about}>
+      <div>
+        <span>About Us</span>
+        <h2>Why will you choose our Services?</h2>
+        <p>
+          Our services go beyond the basic services and provide you with a clean
+          the that will Cleaning services of the reenergize your home and
+          enhance your life.
+        </p>
+        <p>
+          Our services go beyond the basic services and provide you with
+          reenergize your home and enhance your life.
+        </p>
+        <button>Read More</button>
+      </div>
+    </section>
+  );
+}
